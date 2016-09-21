9d9aa2ddc23b7a5aa9555f15e9f31c1d3e4bd3bc
peterkvt80 <peterk.vt80@gmail.com>
2016-09-22 00:16:21 +0100

    Rearranged layout. Implemented swipe and keypad for mobile

--- public/sketch.js
+++ public/sketch.js
@@ -12,6 +12,8 @@ var digit1='1';
 var digit2='0';
 var digit3='0';
 
+var timing=500;
+
 var hdr;
 
 // comms
@@ -22,6 +24,26 @@ var editMode=false;
 
 // dom
 var redButton;
+var indexButton;
+
+
+var btnk0;
+var btnk1;
+var btnk2;
+var btnk3;
+var btnk4;
+var btnk5;
+var btnk6;
+var btnk7;
+var btnk8;
+var btnk9;
+var btnkx;
+var btnky;
+var btnkback;
+var btnkfwd;
+
+// swipe
+var swipeStart;
 
 function preload()
 {
@@ -36,28 +58,30 @@ function setup()
   socket=io.connect(':8080');
 //  socket=io.connect('http://23.251.131.26:8080');
   // socket=io.connect('http://localhost:80');
-  createCanvas(600,550);
-  background(0);
+  var cnv=createCanvas(600,550);
+	cnv.position(0,0);
+	//createCanvas(displayWidth, displayHeight);	
   // font metrics
   textFont(ttxFont);
   textSize(gTtxFontSize);
   gTtxW=textWidth('M');
   gTtxH=gTtxFontSize;
   mypage=new page();
-  mypage.init(100);
+  mypage.init(0x100);
   // message events
   socket.on('keystroke',newChar);
-  socket.on('row',setRow);
-  socket.on('blank',setBlank);
+  socket.on('row',setRow); // A teletext row
+  socket.on('blank',setBlank); // Clear the page
   socket.on('fastext',setFastext);
-  socket.on('setpage',setPageNumber);
+  socket.on('setpage',setPageNumber); // Allow the server to change the page number (Page 404 etc)
   socket.on('description',setDescription);
+  socket.on('subpage',setSubPage); // Subpage number for carousels (Expect two digits 00..99) [99 is higher than actual spec]
   //hdr=new header(0,1,0,0);
   // Set page defaults
   var data=
   {
 	S: 0, // Default to service 0
-	p: 100, // Page mpp
+	p: 0x100, // Page mpp
 	s:0,	// subpage 0
 	x:2000,
 	y: 0,
@@ -73,6 +97,43 @@ function setup()
   yellowButton.mousePressed(fastextY);
   cyanButton=select('#cyan');
   cyanButton.mousePressed(fastextC);
+  //indexButton=select('#index');
+	//indexButton.mousePressed(fastextIndex);
+	
+	// keypad
+  btnk0=select('#k0');
+	btnk0.mousePressed(k0);
+  btnk1=select('#k1');
+	btnk1.mousePressed(k1);
+  btnk2=select('#k2');
+	btnk2.mousePressed(k2);
+  btnk3=select('#k3');
+	btnk3.mousePressed(k3);
+  btnk4=select('#k4');
+	btnk4.mousePressed(k4);
+  btnk5=select('#k5');
+	btnk5.mousePressed(k5);
+  btnk6=select('#k6');
+	btnk6.mousePressed(k6);
+  btnk7=select('#k7');
+	btnk7.mousePressed(k7);
+  btnk8=select('#k8');
+	btnk8.mousePressed(k8);
+  btnk9=select('#k9');
+	btnk9.mousePressed(k9);
+  btnkx=select('#kx');
+	btnkx.mousePressed(kx);
+  btnky=select('#krvl');
+	btnky.mousePressed(krvl);
+  btnkback=select('#kback');
+	btnkback.mousePressed(kback);
+  btnkfwd=select('#kfwd');
+	btnkfwd.mousePressed(kfwd);
+}
+
+function setSubPage(subpage)
+{
+	mypage.setSubPage(parseInt(subpage));
 }
 
 function setDescription(desc)
@@ -84,7 +145,7 @@ function setDescription(desc)
 
 function setPageNumber(data)
 {
-	console.log('[setPageNumber]setting page to '+data.p);
+	console.log('[setPageNumber]setting page to '+data.p.toString(16));
 	mypage.setPage(data.p);
 }
 
@@ -105,6 +166,11 @@ function fastextC()
 {
 	fastext(4);
 }
+
+function fastextIndex()
+{
+	fastext(6);
+}
  
 /**
  * Load the fastext link
@@ -119,39 +185,53 @@ function fastext(index)
 	case 2:page=mypage.greenLink;break;
 	case 3:page=mypage.yellowLink;break;
 	case 4:page=mypage.cyanLink;break;
+	case 6:page=mypage.indexLink;break;
 	default:
 		page=mypage.redLink;
 	}
-	mypage.setPage(page); // We now have a different page number
-	  var data=
-	  {
-		S: 0, // @todo Implement service
-		p: page, // Page mpp
-		s: 0,	// @ todo subpage	
-		x: 0,
-		y: 0,
-		rowText: ''
-	  }	
-	socket.emit('load',data);	
+	console.log('Fastext pressed: '+index+" link to 0x"+page.toString(16)+" ("+page+")");
+	if (page>=0x100 && page<=0x8ff) // Page in range
+	{
+		mypage.setPage(page); // We now have a different page number
+			var data=
+			{
+			S: 0, // @todo Implement service
+			p: page, // Page mpp
+			s: 0,	// @ todo subpage	
+			x: 0,
+			y: 0,
+			rowText: ''
+			}	
+		socket.emit('load',data);	
+	}
 }
 
 function setFastext(data)
 {
   if (!matchpage(data)) return; // Data is not for our page?
-	mypage.redLink=data.fastext[0];
-	mypage.greenLink=data.fastext[1];
-	mypage.yellowLink=data.fastext[2];
-	mypage.cyanLink=data.fastext[3];
+	mypage.redLink=parseInt   ("0x"+data.fastext[0]);
+	mypage.greenLink=parseInt ("0x"+data.fastext[1]);
+	mypage.yellowLink=parseInt("0x"+data.fastext[2]);
+	mypage.cyanLink=parseInt  ("0x"+data.fastext[3]);
+	mypage.indexLink=parseInt ("0x"+data.fastext[5]);
 }
 
 function draw()
 {
+	// No updating while we are pressing the mouse
+	if (swipeStart!=null)
+		return;
   // @todo We only need to update this during updates. No more than twice a second. Could save a lot of CPU
   background(0);
   noStroke();
   fill(255,255,255);
   // ellipse(mouseX,mouseY,10,10);
   mypage.draw();
+	
+	if (timing>0)
+	{
+		timing--;
+	}
 }
 
 // Does our page match the incoming message?
@@ -160,7 +240,7 @@ function matchpage(data)
 	// console.log ("Matching data.p, mypage.pageNumber"+data.p+' '+mypage.pageNumber);
 	if (mypage.service!=data.S) return false;
 	if (mypage.pageNumber!=data.p) return false;
-	if (mypage.subpage!=data.s) return false;
+	// if (mypage.subPage!=data.s) return false; // This needs more thought now that we are implementing carousels
 	return true;
 }
 
@@ -251,62 +331,171 @@ function keyPressed()
  */
 function keyTyped()
 {	
+		processKey(key);
+		return false;
+}
+
+function processKey(keyPressed)
+{
 	if (editMode==true) // Numbers are typed into the page
 	{
-      var data=
-      {
-		S: 0, // service number		
-        p: mypage.pageNumber,
-		s: mypage.subpage,
-        k: key,
-        x: mypage.cursor.x,
-        y: mypage.cursor.y
-      }
-      socket.emit('keystroke', data);
-      newChar(data);
-    }
+		var data=
+		{
+			S: 0, // service number		
+			p: mypage.pageNumber,
+			s: mypage.subpage,
+			k: keyPressed,
+			x: mypage.cursor.x,
+			y: mypage.cursor.y
+		}
+		socket.emit('keystroke', data);
+		newChar(data);
+	}
 	else // Numbers are used for the page selection
 	{
-		if (key>='0' && key <='9')
+		if (keyPressed>='0' && keyPressed <='9')
 		{
-			if (digit1!=' ') // A new number started
+			if (timing==0) // Start a new digit
 			{
-				digit2=' '; // Clear the new page number
-				digit3=' ';
+				digit1='0';
+				digit2='0';
+				digit3='0';
+				timing=500;
 			}
-		    digit1=digit2;
+		  digit1=digit2;
 			digit2=digit3;
-			digit3=key;
+			digit3=keyPressed;
 			if (digit1!=' ')
 			{
-				var page=Number(digit1+digit2+digit3);
-				// hdr.setPage(page);
-				// console.log('@todo: pass the page '+page);
-				console.log("Page number is "+digit1+digit2+digit3);
-				mypage.setPage(page); // We now have a different page number
-				  var data=
-				  {
+				//var page=Number(digit1+digit2+digit3);
+				var page=parseInt("0x"+digit1+digit2+digit3);
+				mypage.pageNumberEntry=digit1+digit2+digit3;
+				if (page>=0x100)
+				{
+					// hdr.setPage(page);
+					// console.log('@todo: pass the page '+page);
+					console.log("Page number is 0x"+page.toString(16));
+					mypage.setPage(page); // We now have a different page number
+					var data=
+					{
 					S: 0, // @todo Implement service
 					p: page, // Page mpp
 					s: 0,	// @ todo subpage	
+					x: 0,
 					y: 0,
 					rowText: ''
-				  }				
-				socket.emit('load',data);
+					}				
+					socket.emit('load',data);
+				}
 			}
+			timimg=500;		
 		}
-	}
-    
+	}    
     // socket.emit('load');
     
-    //console.log('keycode='+key);
-	
+    //console.log('keycode='+keyPressed);
 }
+
+function k0() {	processKey('0'); }
+function k1() {	processKey('1'); }
+function k2() {	processKey('2'); }
+function k3() {	processKey('3'); }
+function k4() {	processKey('4'); }
+function k5() {	processKey('5'); }
+function k6() {	processKey('6'); }
+function k7() {	processKey('7'); }
+function k8() {	processKey('8'); }
+function k9() {	processKey('9'); }
+function kx() {	processKey('x'); } // @todo
+function krvl() {	mypage.toggleReveal() }
+function kback() {prevPage(); }
+function kfwd()  {nextPage(); }
 
 function mouseClicked()
 {
+	// Only need to do this in edit mode
+	if (!editMode)
+		return true;
   var xLoc=int(mouseX/gTtxW);
   var yLoc=int(1+mouseY/gTtxH); // @todo Will need to fix this once we add a header
   mypage.cursor.moveTo(xLoc,yLoc);
   // console.log('The mouse was clicked at '+xLoc+' '+yLoc);
+	return false;
+}
+
+/* Swipes */
+function touchStarted()
+{
+	if (touchY>550) // Only swipe on page
+		return true;
+  console.log('Touch started at '+touchX+' '+touchY);
+	swipeStart=createVector(touchX,touchY);
+	return false;
+}
+
+function touchEnded()
+{
+	if (touchY>550) // Only swipe on page
+		return true;
+  console.log('Touch ended at '+touchX+' '+touchY);
+	var swipeEnd=createVector(touchX,touchY);
+	swipeEnd.sub(swipeStart);
+	swipeStart=null;
+	// Swipe needs to be a minimum distance (& possibly velocity?
+	var mag=swipeEnd.mag();
+	if (mag<40)
+		return false;
+	var heading=swipeEnd.heading();
+	// left
+		console.log("swiped! Heading="+degrees(heading));
+		
+	var dir=4*heading/PI;
+	console.log("Swiped! dir="+dir);
+	if (dir>=-1 && dir<=1)
+	{
+		console.log("swiped right (back)");
+		prevPage();
+	}
+	if (dir>3 || dir<-3)
+	{
+		console.log("swiped left");
+		nextPage();
+	}
+	return false;	
+}
+
+function nextPage()
+{
+	var p=mypage.pageNumber;
+	p++;
+	if (p>0x8fe) p=0x8fe;
+	mypage.setPage(p); // We now have a different page number
+	var data=
+	{
+	S: 0, // @todo Implement service
+	p: p, // Page mpp
+	s: 0,
+	y: 0,
+	rowText: ''
+	}				
+	socket.emit('load',data);		
+	console.log("page="+hex(data.p));
+}
+
+function prevPage()
+{
+	var p=mypage.pageNumber;
+	p--;
+	if (p<0x100) p=0x100;		
+	mypage.setPage(p); // We now have a different page number
+	var data=
+	{
+	S: 0, // @todo Implement service
+	p: p, // Page mpp
+	s: 0,
+	y: 0,
+	rowText: ''
+	}				
+	socket.emit('load',data);		
+	console.log("page="+hex(data.p));
 }
--- public/ttxpage.js
+++ public/ttxpage.js
@@ -14,66 +14,141 @@ function page()
   this.greenLink=100;
   this.yellowLink=100;
   this.cyanLink=100;
+  this.indexLink=100;
   
   this.description='none';
+	
+	this.subPage=0; // This is used to address the sub page as we 
+	this.subPageList=new Array();
+	
+	this.pageNumberEntry='100'; // Page number as entered (used to allow partial page numbers) 
+	
+	this.iteration=0;
+	
+	this.revealMode=false;
 
   // @todo check range
   this.init=function(number)
   {
     this.pageNumber=number;
-	this.subpage=0; // @todo Carousels
-	this.service=0; // @todo Services
+		this.service=0; // @todo Services
+
+		this.addPage(number);
+  }
+	
+	this.toggleReveal=function ()
+	{
+		this.revealMode=!this.revealMode;
+	}
+  
+  /** @brief Change the page number for this page and all child rows
+	 *  Clear the page. We should get a number of rows soon
+   */
+  this.setPage=function(p)
+  {
+		this.subPage=0;
+		this.iteration=0;
+		this.pageNumber=p; /// @todo Convert this to do all sub pages
+		
+		this.pageNumberEntry=p.toString(16);
+		
+		this.subPageList=new Array();
+		this.addPage(this.pageNumber);		
+
+  }
+	
+	/** @brief Add a page to the sub page list
+	 */
+	this.addPage=function(number)
+	{
     this.rows = new Array();
 	// As rows go from 0 to 31 and pages start at 100, we can use the same parameter for both
     this.rows.push(new row(number,0,"Pnn     CEEFAX 1 100 Sun 08 Jan 12:58/57"));
     for (var i=1;i<26;i++)
     {
-      this.rows.push(new row(number,i,"~„~„~„~„~„~„~„~„~„~„~„~„~„~„~„~„~„~„~„~„"));
-    }
-    
-  }
-  
-  /** @brief Change the page number for this page and all child rows
-   */
-  this.setPage=function(p)
-  {
-	this.pageNumber=p;
-	for (var r=0;r<this.rows.length;r++)
-	{
-		this.rows[r].page=p;
+      //this.rows.push(new row(number,i,"~„~„~„~„~„~„~„~„~„~„~„~„~„~„~„~„~„~„~„~„"));
+      this.rows.push(new row(number,i,"                                        "));
+    }  
+		this.subPageList.push(this.rows);
 	}
-  }
+	
+	/**
+   * @param s Subpage number. All subsequent row/char updates go to this subpage.	
+	 */
+	this.setSubPage=function(s)
+	{
+		s=parseInt(s);
+		// console.log("[setSubPage] enters "+s);
+		if (s<0 || s>99)
+			s=0;
+		///@todo Check that s is in a subpage that exists and add it if needed.
+		if (this.subPageList.length<s)
+		{
+			this.addPage(this.pageNumber);
+			// console.log("[setSubPage] Need to add a new subpage: "+s);
+		}
+		this.subPage=s;
+	}
 
   /** @brief Set row r to txt
-   */
-   
+   */   
   this.setRow=function(r,txt)
   {
-	if (r>=0 && r<=24)
-	{
-		this.rows[r].setrow(txt);
-	}
-	else
-		console.log('not setting row '+r+' to '+txt);
+		if (r>=0 && r<=24)
+		{
+			//this.rows[r].setrow(txt); // Old working code
+			// console.log ("Adding row "+r+" to sub page "+this.subPage);
+			var sp=this.subPage;
+			if (sp>0) sp=sp-1;
+			var v=this.subPageList[sp];
+			//console.log("v="+v);
+			v[r].setrow(txt); // New not tested code
+		}
+		else
+			return;
+		/*
+			// Might want to find out why this happens. Doesn't seem to matter
+			console.log('not setting row '+r+' to '+txt);
+			*/
   }
   
   
   this.draw=function()
   {
+		this.iteration++;
+		var dblHeight;
+		if (this.iteration % 30==0) // Approx 7 seconds
+		{
+			this.subPage=(this.subPage+1) % this.subPageList.length;			
+			// console.log("iteration="+this.iteration/30+" Subpage="+this.subPage);
+		}
     for (var row=0;row<this.rows.length;row++)
     {
 	// console.log("drawing row "+row+" of "+this.rows.length);
       var cpos=-1;
-	  if (!this.cursor.hide && row==this.cursor.y) // If in edit mode and it is the correct row...
-		cpos=this.cursor.x;
-      this.rows[row].draw(cpos);
-    }    
+			if (!this.cursor.hide && row==this.cursor.y) // If in edit mode and it is the correct row...
+			cpos=this.cursor.x;
+			//this.rows[row].draw(cpos); // Original version
+			// Single pages tend to have subpage 0000. carousels start from 0001. So subtract 1 unless it is already 0.
+			var v=this.subPageList[this.subPage>0?this.subPage-1:0];
+			
+			if (v.length>0)
+			{			
+				if (row==0 && v.length>0) // Set the page number for the header only
+				{
+					v[0].setpagetext(this.pageNumberEntry);
+				}
+
+				if (v[row].draw(cpos, this.revealMode))
+					row++; // If double height, skip the next row 
+			}    
+		}
   }
   
   this.drawchar=function(ch,x,y)
   {
     //console.log('Attempting to draw row '+y);
-    this.rows[y].setchar(ch,x);
+    this.rows[y].setchar(ch,x); /// @todo Convert this to update subPage
   }
   
   /**
@@ -81,20 +156,31 @@ function page()
    */
   this.setBlank=function()
   {
-    for (var y=1;y<this.rows.length;y++)
-		this.rows[y].setrow('                                        ');
-	this.rows[0].setrow('Pnn     CEEFAX 1 100 Sun 08 Jan 12:58/57'); // @todo Add proper header control		
+		console.log(" Clear all rows to blank");
+		this.subPageList=new Array();
+		this.addPage(this.pageNumber);
+		
+//    for (var y=1;y<this.rows.length;y++)
+			//this.rows[y].setrow('                                        ');
+		//this.rows[0].setrow('Pnn     CEEFAX 1 100 Sun 08 Jan 12:58/57'); // @todo Add proper header control		
   }
   
   
 } // page
+
+function isMosaic(ch)
+{
+    ch=ch.charCodeAt() & 0x7f;
+    return (ch>=0x20 && ch<0x40) || ch>=0x60;
+}
 
 function row(page,y,str)
 {
   this.page=page;
   this.row=y;
   this.txt=str;
-  console.log('Setting row to '+this.page+' '+this.row+' '+this.txt);
+	this.pagetext='xxx';
+  // console.log('Setting row to '+this.page+' '+this.row+' '+this.txt);
   this.setchar=function(ch,n)
   {
     this.txt=setCharAt(this.txt,n,ch);
@@ -104,22 +190,32 @@ function row(page,y,str)
   {
     this.txt=txt;
   }
+	
+	/** Expect a three digit page number, or partial page number */
+	this.setpagetext=function(txt)
+	{
+		this.pagetext=txt;
+	}
 
-  /// @param cpos is the cursor column position to highlight
-  this.draw=function(cpos)
+  /** @param cpos is the cursor column position to highlight
+	 *  @param if revealMode is true overrides conceal 
+	 * @return True if there was a double height code in this row
+	 */
+  this.draw=function(cpos, revealMode)
   {
 	var txt=this.txt; // Copy the row text because a header row will modify it
 	if (this.row==0 && cpos<0) // This is the header row
 	{
 		// Replace the first eight characters with the page number
-		txt=replace(txt,'P'+nf(this.page,3)+'    ',0);
+//		txt=replace(txt,'P'+this.page.toString(16)+'    ',0);
+		txt=replace(txt,'P'+this.pagetext+'    ',0);
 		
 		// Substitute mpp for the page number
 		var ix;
 		if ((ix=txt.indexOf('mpp'))>0)
-			txt=replace(txt,nf(this .page,3),ix)
+			txt=replace(txt,this.page.toString(16),ix)
 		// Substitute dd for the day 1..31
-		if (ix=txt.indexOf('dd'))
+		if ((ix=txt.indexOf('dd'))>0)
 			txt=replace(txt,nf(day(),2),ix)
 		// Substitute DAY for the three letter abbreviated day 
 		ix=txt.indexOf('DAY');
@@ -146,45 +242,155 @@ function row(page,y,str)
 		if ((ix=txt.indexOf('ss'))>0)
 			txt=replace(txt,nf(second(),2),ix)
 	}
+	// Non header substitutions
+	if (this.row>0 && this.row<25 && cpos<0) // This is the header row
+	{
+		// World time. (two values allowed per line
+  	for (var i=0;i<2;i++)
+			if ((ix=txt.indexOf('%t'))>0)
+			{
+				// Read the half hour offsets
+				var offset=txt.substring(ix+2,ix+5);
+				// console.log(offset);
+				// add the offset to the time
+				// show the HH:MM
+				var h=(hour()+int(parseInt(offset)/2)) % 24;
+				var m=minute();
+				if ( (parseInt(offset) % 2)==1)
+					m=(m+offset*30 ) % 60;
+				txt=replace(txt,nf(h,2)+':'+nf(m,2),ix);
+			}
+	}
     // Set up all the display mode initial defaults
     var fgColor=color(255,255,255); // Foreground defaults to white
     var bgColor=color(0); // Background starts black
     var textmode=true; // If false it is graphics mode
     var contiguous=true; // if false it is separated graphics
+		var concealed=false;
     var holdGfx=false;
+		var holdChar=' ';
     var flashMode=false;
     var dblHeight=false;
+		var hasDblHeight=false; // If there is a double height anywhere on this row, the next row must be skipped.
     textFont(ttxFont);
     textSize(gTtxFontSize);
     for (var i=0;i<40;i++)
     {
       var ch=txt.charAt(i);
-      var ic=txt.charCodeAt(i);
+      var ic=txt.charCodeAt(i) & 0x7f;
       var printable=false;
+			// Do the set-before codes
       switch (ic)
       {
-      case  0 : fgColor=color(0);textmode=true;break; // 0: black. Only for level 1 rebels.
-      case  1 : fgColor=color(255,0,0);textmode=true;break; // 1:red 
-      case  2 : fgColor=color(0,255,0);textmode=true;break; // 2:green
-      case  3 : fgColor=color(255,255,0);textmode=true;break; // 3:yellow
-      case  4 : fgColor=color(0,0,255);textmode=true;break; // 4:blue
-      case  5 : fgColor=color(255,0,255);textmode=true;break; // 5:magenta
-      case  6 : fgColor=color(0,255,255);textmode=true;break; // 6:cyan
-      case  7 : fgColor=color(255,255,255);textmode=true;break; // 7:white
-      case  8 : flashMode=true;break; // 8:flash
+      case  0 : ; // 0: black. Only for level 1 rebels.
+      case  1 : ; // 1:red 
+      case  2 : ; // 2:green
+      case  3 : ; // 3:yellow
+      case  4 : ; // 4:blue
+      case  5 : ; // 5:magenta
+      case  6 : ; // 6:cyan
+      case  7 : ; // 7:white
+				holdGfx=false;
+				concealed=false;
+				break;
+      case  8 : flashMode=true; // 8:flash
+				break;
       case  9 : flashMode=false;break; // 9:steady
       case 10 : break; // 10:endbox
       case 11 : break; // 11:startbox
-      case 12 :
+      case 12 : // 12:normalheight SetAt
+			break;//////////////////
         dblHeight=false;
         textFont(ttxFont);
         textSize(gTtxFontSize);
-        break; // 12:normalheight
-      case 13 :
-        dblHeight=true;
+        break; 
+      case 13 : // 13:doubleheight SetAfter
+				break;
+      case 16 : ;// 16: Farrimond gfxblack
+      case 17 : ; // 16:gfxred 
+      case 18 : ; // 17:gfxgreen
+      case 19 : ; // 18:gfxyellow
+      case 20 : ; // 19:gfxblue
+      case 21 : ; // 20:gfxmagenta
+      case 22 : ; // 21:gfxcyan
+      case 23 : ; // 22:gfxwhite
+				concealed=false;
+				break;
+			case 24 :  // 24: conceal. (SetAt)
+				if (!revealMode) concealed=true;
+				break;
+      case 25 : contiguous=true;break; // 25: Contiguous graphics
+      case 26 : contiguous=false;break; // 26: Separated graphics
+
+      case 28 : bgColor=color(0);break; // 28 black background
+      case 29 : bgColor=fgColor;break; // 29: new background
+      case 30 : holdGfx=true; // 30: Hold graphics mode (set at)
+      	printable=true; // Because this will be replaced
+      	break;
+      case 31 : break; // 31 Release hold mode (set after)
+	  case 32 : ; // Space is not printable but it is still a mosaic. Intentional fall through
+      default:
+			  if (isMosaic(ch))
+				{
+					holdChar=ic;
+				}
+			
+        printable=true;
+      } // case
+			
+			// Mosaic hold is always printable
+			if (!textmode && holdGfx)
+        printable=true;
+
+				// Paint the background colour always
+      noStroke();
+      fill(bgColor);
+      // except if this is the cursor position
+      if (cpos==i && flashState) fill(255);
+      this.drawchar(String.fromCharCode(0xe6df),i,this.row,dblHeight);
+      if (printable && (flashState || !flashMode) && !concealed) 
+      {
+        fill(fgColor);
+        if (textmode || (ch>='A' && ch<='Z'))
+        {
+          ch=this.mapchar(ch);
+          this.drawchar(ch,i,this.row,dblHeight);
+        }
+        else // mosaics
+        {
+		  
+          fill(fgColor);
+					var ic2=ic;
+					if (holdGfx)
+						ic2=holdChar; // hold char replaces
+          if (contiguous)
+          {
+            stroke(fgColor);
+            this.drawchar(String.fromCharCode(ic2+0x0e680-0x20),i,this.row,dblHeight);
+          }
+          else
+          {
+            this.drawchar(String.fromCharCode(ic2+0x0e680),i,this.row,dblHeight);
+          }
+        }        
+      }
+			// Set-After codes go here
+			switch (ic)
+			{
+			case  0 : fgColor=color(0);textmode=true;break; // 0: black. Only for level 1 rebels.
+			case  1 : fgColor=color(255,0,0);textmode=true;break; // 1:red 
+			case  2 : fgColor=color(0,255,0);textmode=true;break; // 2:green
+			case  3 : fgColor=color(255,255,0);textmode=true;break; // 3:yellow
+			case  4 : fgColor=color(0,0,255);textmode=true;break; // 4:blue
+			case  5 : fgColor=color(255,0,255);textmode=true;break; // 5:magenta
+			case  6 : fgColor=color(0,255,255);textmode=true;break; // 6:cyan
+			case  7 : fgColor=color(255,255,255);textmode=true;break; // 7:white	
+			case 13 : // 13: double height
+  			dblHeight=true;
+				hasDblHeight=true;
         textFont(ttxFontDH);
-        textSize(gTtxFontSize*2);
-        break; // 13:doubleheight
+        textSize(gTtxFontSize*2); 
+        break; 
       case 16 : fgColor=color(0);textmode=false;break;// 16: Farrimond gfxblack
       case 17 : fgColor=color(255,0,0);textmode=false;break; // 16:gfxred 
       case 18 : fgColor=color(0,255,0);textmode=false;break; // 17:gfxgreen
@@ -192,53 +398,21 @@ function row(page,y,str)
       case 20 : fgColor=color(0,0,255);textmode=false;break; // 19:gfxblue
       case 21 : fgColor=color(255,0,255);textmode=false;break; // 20:gfxmagenta
       case 22 : fgColor=color(0,255,255);textmode=false;break; // 21:gfxcyan
-      case 23 : fgColor=color(255,255,255);textmode=false;break; // 22:gfxwhite
-
-      case 25 : contiguous=true;break; // 25: Contiguous graphics
-      case 26 : contiguous=false;break; // 26: Separated graphics
-
-      case 28 : bgColor=color(0);break; // 28 black background
-      case 29 : bgColor=fgColor;break; // 29: new background
-      case 30 : holdGfx=true;break; // 30: Hold graphics mode
-      case 31 : holdGfx=false;break; // 31 Release hold mode
-	  case 32 : break; // Space is not printable
-      default:
-        printable=true;
-      } // case
-      // Paint the background colour always
-      noStroke();
-      fill(bgColor);
-      // except if this is the cursor position
-      if (cpos==i && flashState) fill(255);
-      this.drawchar(String.fromCharCode(0xe6df),i,this.row);
-      if (printable && (flashState || !flashMode)) 
-      {
-        fill(fgColor);
-        if (textmode || ch>='A' && ch<='Z')
-        {
-          ch=this.mapchar(ch);
-          this.drawchar(ch,i,this.row);
-        }
-        else
-        {
-		  
-          fill(fgColor);
-          if (contiguous)
-          {
-            stroke(fgColor);
-            this.drawchar(String.fromCharCode(ic+0x0e680-0x20),i,this.row);
-          }
-          else
-          {
-            this.drawchar(String.fromCharCode(ic+0x0e680),i,this.row);
-          }
-        }        
-      }
+      case 23 : fgColor=color(255,255,255);textmode=false;break; // 22:gfxwhite			
+			case 24 : break;// 24:conceal
+      case 31 : holdGfx=false;break; // 31 Release hold mode (set after)
+			
+			}
     }
-  }
-  this.drawchar=function(ch,x,y)
+		if (this.row<1 || this.row>22) // Can't double height header or last row.
+			return;
+		else
+			return hasDblHeight;
+  } // draw
+	
+  this.drawchar=function(ch,x,y,dblH)
   {
-    text(ch,x*gTtxW,(y+1)*gTtxH);
+    text(ch,x*gTtxW,(y+1+(dblH?1:0))*gTtxH);
   }
   this.mapchar=function(ch)
   {
@@ -261,6 +435,7 @@ switch(ch)
     case '|':  return char(0x2016); // 7/C Double pipe
     case '}':  return char(0xbe);   // 7/D Three quarters
     case '~':  return char(0x00f7); // 7/E Divide 
+		case String.fromCharCode(0x7f):  return char(0xe65f); // 7/F Bullet (rectangle block)
     }
 	
     return ch;
