bf50cceb37710e0799d6f1bbfa829c9bcaf25b7a
Danny Allen <me@dannya.com>
2021-09-23 17:53:19 +0100

    * Implement centralised (shared across backend and frontend) constants and configuration system (set via config.js). * More code cleanup, including moving options into `config.js` to allow for customisation. * Implement option to serve via HTTPS (#2). * Allow a non-`TEEFAX` custom header title (as defined in config.js) to be substituted in at page render time. * Render index.html via Nunjucks templating system (allows us to pass in config at render time).

--- public/edittest.js
+++ public/edittest.js
@@ -5,15 +5,16 @@ const EDITMODE_EDIT=1   // edit mode
 const EDITMODE_ESCAPE=2 // expect next character to be either an edit.tf function or Escape again to exit.
 const EDITMODE_INSERT=3 // The next character is ready to insert
 
-require('./edittf.js')
-require('./cursor.js')
-require('./ttxpage.js')
+require('./edittf.js');
+require('./cursor.js');
+require('./ttxpage.js');
+
 
 console.log("Test configuring")
 
-var page=new TTXPAGE()
-  page.init(0x190)
-  page.setSubPage(0001)
+var page=new TTXPAGE();
+page.init(0x190);
+page.setSubPage(0001);
 
 // var website="http://edit.tf" // could be zxnet. Up to you
 var website="https://zxnet.co.uk/teletext/editor/" // could be zxnet. Up to you
